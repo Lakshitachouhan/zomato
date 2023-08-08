b27772ed67131e91cbbe2be7a9ba30de9a4d625d
Lakshitachouhan <chouhanlakshita18@gmail.com>
2023-08-08 16:55:11 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$('.sliders .owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+
+$('.sliders2 .owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+})
